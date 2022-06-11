bf111825fe12b1519a0199dbbf62b85620cfaa03
Alex <bbqsauce108@gmail.com>
2022-06-11 15:44:11 +0300

    slightly change blogger id validation to pass tests

--- src/middlewares/bloggers/blogger-id-validation-middleware.ts
+++ src/middlewares/bloggers/blogger-id-validation-middleware.ts
@@ -1,5 +1,12 @@
 import {body} from "express-validator";
+import {findBlogger} from "../../repository/bloggers-repository";
 
 export const bloggerIdValidationMiddleware = body('bloggerId')
     .isNumeric()
     .withMessage('blogger id should be a number')
+    .custom(value =>{
+        const bloggerExists = findBlogger(value)
+        if(!bloggerExists){
+            return Promise.reject('Blogger doesn\'t exist')
+        }
+})
