835dba40e86c0e8bf45d2f6c6de913124006e149
Alex <bbqsauce108@gmail.com>
2022-06-11 15:28:11 +0300

    change post content validation restrictions

--- src/middlewares/posts/content-validation-middleware.ts
+++ src/middlewares/posts/content-validation-middleware.ts
@@ -1,6 +1,6 @@
 import {body} from "express-validator";
 
 export const contentValidationMiddleware = body('content').trim().isLength(({
-    min: 10,
+    min: 5,
     max: 1000
-})).withMessage('content should be min 10 and at most 1000 characters')
+})).withMessage('content should be min 5 and at most 1000 characters')
